76674e6a744b537d733fd28037f62e1483e37c92
Monish S <srinivasanseene4@gmail.com>
2026-03-05 14:12:16 +0530

    Create WatchFaceComposable.kt for watch face rendering

---
+++ app/src/main/java/com/kern/ui/composables/WatchFaceComposable.kt
@@ -0,0 +1,16 @@
+package com.kern.ui.composables
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun WatchFaceComposable() {
+    // TODO: Implement watch face rendering logic
+    // This is where you'll add the drawing and UI components for the watch face.
+}
+
+@Preview
+@Composable
+fun PreviewWatchFace() {
+    WatchFaceComposable()
+}
